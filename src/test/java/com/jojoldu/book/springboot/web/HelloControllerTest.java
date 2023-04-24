7eccc8577aa144bcf98b66e864e375ed1ceb269e
dorameme <doradorav@naver.com>
2023-04-24 15:32:28 +0900

    jpa다루기 1

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,21 +1,23 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import com.jojoldu.book.springboot.web.HelloController;
+import junit.framework.TestCase;
+import org.junit.Test;//1
+import org.junit.runner.RunWith;//2
+import org.springframework.beans.factory.annotation.Autowired;//3
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;//4
+import org.springframework.test.context.junit4.SpringRunner;//5
+import org.springframework.test.web.servlet.MockMvc;//6
+import org.springframework.test.web.servlet.ResultActions;//7
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
@@ -33,10 +35,11 @@ public class HelloControllerTest {
         String name="hello";
         int amount=1000;
         mvc.perform(
-                get("/hello/dto").param("name",name)
-                        .param("amount",String.valueOf(amount)))
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name",is(name)))
-                .andExpect(jsonPath("$.amount",is(amount)));
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
